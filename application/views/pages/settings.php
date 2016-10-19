1d849bd6772fee282defe5d705baffdf1fd458ad
Crisnil <cacuyado@gmail.com>
2016-10-19 16:51:40 +0800

    update

--- application/views/pages/settings.php
+++ application/views/pages/settings.php
@@ -1,13 +1,76 @@
+<hr>
 <div class="row">
-	<div class="user-dashboard">
-		<pre>
-		Notification
-		Update to pro user
-		Change password
-		Mail
-		Billing adresse
-		Abonnement type
-		Filter options
-		</pre>
-	</div>
+
+<div class="user-dashboard">
+	
+	<div class="row">
+		<div class="col-md-12">
+			<div class="row">
+				
+					
+							<div class="col-md-4">
+								<div class="panel panel-default">
+									<div class="panel-body">
+									<div class="page-header">
+									  <h3>Upgrage to pro user</h3>
+									</div>
+										<div class="input-group">
+												  <button class="btn btn-primary btn-lg">Upgrade now!</button>
+										</div>
+										<p>
+										Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+										</p>
+									</div>
+								</div>
+									
+							</div>
+				
+							<div class="col-md-4">
+								<ul class="list-group">
+								  <li class="list-group-item">Cras justo odio</li>
+								  <li class="list-group-item">Dapibus ac facilisis in</li>
+								  <li class="list-group-item">Morbi leo risus</li>
+								  <li class="list-group-item">Porta ac consectetur ac</li>
+								  <li class="list-group-item">Vestibulum at eros</li>
+								</ul>
+									<div class="input-group">
+											  <span class="input-group-addon">
+												<input type="checkbox" aria-label="...">
+											  </span>
+											  <input type="text" class="form-control" aria-label="..." disabled value="Notif updates">
+									</div><!-- /input-group -->
+									 <div class="input-group">
+											  <span class="input-group-addon">
+												<input type="checkbox" aria-label="...">
+											  </span>
+											  <input type="text" class="form-control" aria-label="..." disabled value="Notify me if I won the bid">
+									</div><!-- /input-group -->
+									<div class="input-group">
+											  <span class="input-group-addon">
+												<input type="checkbox" aria-label="...">
+											  </span>
+											  <input type="text" class="form-control" aria-label="..." disabled value="Notify me if I lose the bid">
+									</div><!-- /input-group -->	
+								
+									
+							</div>
+							<div class="col-md-4">
+								<div class="panel panel-default">
+								  <div class="panel-heading">Panel heading without title</div>
+								  <div class="panel-body">
+									<div class="input-group">
+											  <span class="input-group-addon">
+												<input type="checkbox" aria-label="...">
+											  </span>
+											  <input type="text" class="form-control" aria-label="..." disabled value="Notify Me via email">
+										</div><!-- /input-group -->
+								  </div>
+								</div>
+									
+							</div>
+			
+		</div>
+	</div>		
+
+</div>
 </div>
